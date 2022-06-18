0ad545d825729accc372dd43725eb50e153052e2
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-06-18 00:57:43 -0300

    ADD --- Firebase hosting config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIK4Nfk1iG-MTVtPZ1P4jBWYY06FNJqhs",
+  authDomain: "portfolioglopezfrutos.firebaseapp.com",
+  projectId: "portfolioglopezfrutos",
+  storageBucket: "portfolioglopezfrutos.appspot.com",
+  messagingSenderId: "639583349601",
+  appId: "1:639583349601:web:878588fc7864e3ad47f320"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
