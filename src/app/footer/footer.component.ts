776364ba5014b9e1d5e62d2be3527b5f24cb5280
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-07-28 22:11:54 -0300

    ADD --- Footer  fa icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  faLinkedin = faLinkedin
+  faGithub = faGithub
 
   constructor() { }
 
