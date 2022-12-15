c81d16f4755ee2a0d67570728ef4ce5e836632b5
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-12-15 14:56:24 -0300

    FIX Header Carousel

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: ''});
 
   constructor() { }
 
   ngOnInit(): void {
+    this.slides[0] = {
+      src: "../../assets/logoSofkaU.webp",
+      title: "Logo Sofka U"
+    };
+    this.slides[1] = {
+      src: "../../assets/logoSofka.png",
+      title: "Logo Sofka"
+    }
+    this.slides[2] = {
+      src: "../../assets/logoArgentinaPrograma.png",
+      title: "Logo Argentina Programa"
+    }
   }
-
 }
