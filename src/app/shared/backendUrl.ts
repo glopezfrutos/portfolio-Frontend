5e8137820ea3f47eb9a9d7b0eec783f228992fe6
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-12-21 09:28:12 -0300

    FIX endpoint updated

--- src/app/shared/backendUrl.ts
+++ src/app/shared/backendUrl.ts
@@ -1,2 +1,4 @@
-// export const backendUrl = 'http://localhost:8080/api/v1';
-export const backendUrl = 'https://glopezfrutos.herokuapp.com/api/v1';
+export const backendUrl =
+    // 'http://localhost:8080/api/v1';
+    // 'https://glopezfrutos.herokuapp.com/api/v1';
+    'https://portfolio-back-guslopezfrutos.koyeb.app/api/v1';
